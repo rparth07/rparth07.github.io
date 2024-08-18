f06018e619feeb9c6668d25060dc60ff46312120
rparth07 <pmr642001@gmail.com>
2024-08-18 22:52:42 +0530

    modified about me, skills, experience, projects section and made it responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -162,19 +162,20 @@
   /**
    * Skills animation
    */
-  let skilsContent = select(".skills-content");
-  if (skilsContent) {
-    new Waypoint({
-      element: skilsContent,
-      offset: "80%",
-      handler: function (direction) {
-        let progress = select(".progress .progress-bar", true);
-        progress.forEach((el) => {
-          el.style.width = el.getAttribute("aria-valuenow") + "%";
-        });
-      },
-    });
-  }
+  // TODO: Need to check the animation and use it if useful
+  // let skilsContent = select(".skills-content");
+  // if (skilsContent) {
+  //   new Waypoint({
+  //     element: skilsContent,
+  //     offset: "80%",
+  //     handler: function (direction) {
+  //       let progress = select(".progress .progress-bar", true);
+  //       progress.forEach((el) => {
+  //         el.style.width = el.getAttribute("aria-valuenow") + "%";
+  //       });
+  //     },
+  //   });
+  // }
 
   /**
    * Porfolio isotope and filter
@@ -246,20 +247,20 @@
   /**
    * Testimonials slider
    */
-  new Swiper(".testimonials-slider", {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-  });
+  // new Swiper(".testimonials-slider", {
+  //   speed: 600,
+  //   loop: true,
+  //   autoplay: {
+  //     delay: 5000,
+  //     disableOnInteraction: false,
+  //   },
+  //   slidesPerView: "auto",
+  //   pagination: {
+  //     el: ".swiper-pagination",
+  //     type: "bullets",
+  //     clickable: true,
+  //   },
+  // });
 
   /**
    * Animation on scroll
